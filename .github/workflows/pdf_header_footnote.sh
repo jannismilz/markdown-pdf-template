7bc8d87b85d52cbc87bc293046be0a003b5477d5
JannisMilz <jannis@milz.ch>
2023-08-30 20:34:34 +0200

    Test again

--- .github/workflows/pdf_header_footnote.sh
+++ .github/workflows/pdf_header_footnote.sh
@@ -6,11 +6,9 @@ sudo apt-get install pdftk paps -y
 input_pdf="pdfs/output.pdf"
 output_pdf="pdfs/final.pdf"
 temp_dir="temp_pages"
-output_dir="output_pdfs"
 
-# Create temporary and output directories if they don't exist
+# Create a temporary directory if it doesn't exist
 mkdir -p "$temp_dir"
-mkdir -p "$output_dir"
 
 # Get total number of pages
 total_pages=$(pdftk "$input_pdf" dump_data | grep "NumberOfPages" | awk '{print $2}')
@@ -27,14 +25,11 @@ for ((page=1; page<=total_pages; page++)); do
     pdftk "$temp_page" stamp <(echo "[$page / $total_pages]" | \
         paps --bottom-margin=10 --font="Helvetica 8") output "$temp_page"
 
-    # Move processed page to the output directory
-    mv "$temp_page" "$output_dir/"
-
     # Concatenate pages
     if [[ $page -eq 1 ]]; then
-        cp "$output_dir/temp_page_$page.pdf" "$output_pdf"
+        cp "$temp_page" "$output_pdf"
     else
-        pdftk "$output_pdf" "$output_dir/temp_page_$page.pdf" cat output "$output_pdf"
+        pdftk "$output_pdf" "$temp_page" cat output "$output_pdf"
     fi
 done
 
