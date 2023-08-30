092e875553b2ba5fa6e0598b2f8d4289a66307e7
JannisMilz <jannis@milz.ch>
2023-08-30 20:41:21 +0200

    Test again

--- .github/workflows/pdf_header_footnote.sh
+++ .github/workflows/pdf_header_footnote.sh
@@ -16,21 +16,25 @@ total_pages=$(pdftk "$input_pdf" dump_data | grep "NumberOfPages" | awk '{print 
 # Loop through each page
 for ((page=1; page<=total_pages; page++)); do
     # Create a temporary file for the modified page
-    temp_page="$temp_dir/temp_page_$page.pdf"
+    temp_input="$temp_dir/temp_input_$page.pdf"
+    temp_output="$temp_dir/temp_output_$page.pdf"
 
-    # Add pagination to the current page
-    # pdftk "$input_pdf" cat $page output "$temp_page"
+    # Extract the current page to a temporary file
+    pdftk "$input_pdf" cat $page output "$temp_input"
 
     # Add page number to the bottom right corner
-    pdftk "$temp_page" stamp <(echo "[$page / $total_pages]" | \
-        paps --bottom-margin=10 --font="Helvetica 8") output "$temp_page"
+    pdftk "$temp_input" stamp <(echo "[$page / $total_pages]" | \
+        paps --bottom-margin=10 --font="Helvetica 8") output "$temp_output"
 
     # Concatenate pages
     if [[ $page -eq 1 ]]; then
-        cp "$temp_page" "$output_pdf"
+        cp "$temp_output" "$output_pdf"
     else
-        pdftk "$output_pdf" "$temp_page" cat output "$output_pdf"
+        pdftk "$output_pdf" "$temp_output" cat output "$output_pdf"
     fi
+
+    # Clean up temporary files
+    rm "$temp_input" "$temp_output"
 done
 
 # Clean up temporary directory
